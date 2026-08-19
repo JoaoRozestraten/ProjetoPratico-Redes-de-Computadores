68dc1ac9f45b6bd472af96e914d808bbf6e6fdc3
Rodrigo Seiji Yugoshi Ushizawa <rodrigo.syu@puccampinas.edu.br>
2026-08-19 10:20:52 -0300

    feat: dict/registro de possiveis commandos mapeados com suas callbacks

--- src/server/ReceiverThread.py
+++ src/server/ReceiverThread.py
@@ -1,21 +1,27 @@
 import threading
-import socket
+from queue import Queue
+from typing import Callable
+from monitor.Monitor import Monitor
+import time
+
+_REGESTRY: dict[str, Callable] = {
+    # "CPU": cpuConsumption,
+    # "RAM": ramConsumption,
+    # "EXIT":  # deletes a monitor
+}
 
 
 class ReceiverThread(threading.Thread):
-    def __init__(self, conn, addr, shared_q) -> None:
+    def __init__(self, conn, addr, monitor_q: Queue) -> None:
         super().__init__()
         self.conn = conn
         self.addr = addr
-        self.shared_q = shared_q
+        self.monitor_q = monitor_q
 
     def run(self):
-        with self.conn:
-            print(f"Connected to {self.addr}")
-            while True:
-                self.data = self.conn.recv(1024)
-                if not self.data:
-                    print(f"Disconnected from {self.addr}")
-                    break
-
-                self.conn.sendall(self.data)
+        print(f"Connected to {self.addr}")
+        while True:
+            data = self.conn.recv(1024)
+            if not data:
+                print(f"Disconnected from {self.addr}")
+                break
