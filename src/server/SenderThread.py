250c149a21c43035cd74ef5c04ee48e8b99e951c
Rodrigo Seiji Yugoshi Ushizawa <rodrigo.syu@puccampinas.edu.br>
2026-08-19 10:49:44 -0300

    feat: task scheduler

--- src/server/SenderThread.py
+++ src/server/SenderThread.py
@@ -1,11 +1,27 @@
+from queue import Queue
 import threading
-import socket
+from server.monitor.Monitor import Monitor
+import time
 
 
 class SenderThread(threading.Thread):
-    def __init__(self, conn, addr) -> None:
+    def __init__(self, conn, monitor_q: Queue[Monitor]) -> None:
         super().__init__()
         self.conn = conn
+        self.monitor_q = monitor_q
 
     def run(self):
-        pass
+        while True:
+            curr = self.monitor_q.get()
+            tempo = time.time()
+
+            if curr.state == "PRONTO":
+                message = curr.callback()
+                curr.next_time = tempo + curr.period
+                self.conn.send(message)
+                curr.state = "ESPERA"
+            if curr.state == "ESPERA":
+                if curr.next_time <= tempo:
+                    curr.state = "PRONTO"
+            if curr.state != "QUIT":
+                self.monitor_q.put(curr)
